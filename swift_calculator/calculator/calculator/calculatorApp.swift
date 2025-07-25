ec8fdd20b34a12f6beb27c055af8f29af61a04eb
Jose Zarabanda <jlzarabandad@gmail.com>
2025-07-25 05:45:26 -0400

    Add initial project files and setup for calculator app

---
+++ swift_calculator/calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by JOSE ZARABANDA on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
